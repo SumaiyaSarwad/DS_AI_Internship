d77edb6c82f41a936f2509ce61e7fc7c50d61286
Sumaiya Sarwad <sumaiyansarwad06@gmail.com>
2026-02-04 22:59:56 +0530

    deleted:    Day 3/Day 3_fundamentals.py                   Day 3/Task_3_The Immutable Config (Tuples vs Lists).py         Day 3/day_3_collections.py

---
+++ src/Day 3/Task_3_The Immutable Config (Tuples vs Lists).py
@@ -0,0 +1,13 @@
+#Task 3: The Immutable Config (Tuples vs Lists)
+screen_res = (1920, 1080)
+print("Current Resolution: 1920*1080")
+
+#screen_res[0] = 1280
+
+print("Tuples cannot be modified!")
+
+
+#Output: Current Resolution: 1920*1080
+#        TypeError: 'tuple' object does not support item assignment
+#        Tuples cannot be modified!
+
